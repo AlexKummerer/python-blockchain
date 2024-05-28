f08a6344a9dae821e25f59d4b765fcd28802af54
alexkummerer <alexander@thomas-kummerer.de>
2024-05-28 18:41:42 +0200

    using defaul argues

--- blockchain.py
+++ blockchain.py
@@ -1,10 +1,15 @@
-blockchain = [[1]]
+blockchain = []
+
 
 def get_last_blockchain_value():
     return blockchain[-1]
 
-def add_value(transaction_amount):
-    blockchain.append([get_last_blockchain_value(), transaction_amount])
+
+def add_value(transaction_amount, last_transaction = [1]):
+    blockchain.append([last_transaction,  transaction_amount])
     print(blockchain)
-    
+
+
 add_value(5.4)
+add_value(0.1, get_last_blockchain_value())
+add_value(49.3, get_last_blockchain_value())
