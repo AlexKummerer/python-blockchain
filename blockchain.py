77b7201acd4e1d96661b8ab975a8a6ca6c343931
alexkummerer <alexander@thomas-kummerer.de>
2024-05-28 18:33:42 +0200

    return last blockchain value

--- blockchain.py
+++ blockchain.py
@@ -1,7 +1,10 @@
 blockchain = [[1]]
 
+def get_last_blockchain_value():
+    return blockchain[-1]
+
 def add_value(transaction_amount):
-    blockchain.append([blockchain[-1], transaction_amount])
+    blockchain.append([get_last_blockchain_value(), transaction_amount])
     print(blockchain)
     
 add_value(5.4)
