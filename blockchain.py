6a4349f0d112e76895168734e3d0141602f520a2
alexkummerer <alexander@thomas-kummerer.de>
2024-05-28 18:47:04 +0200

    using input

--- blockchain.py
+++ blockchain.py
@@ -5,11 +5,17 @@ def get_last_blockchain_value():
     return blockchain[-1]
 
 
-def add_value(transaction_amount, last_transaction = [1]):
-    blockchain.append([last_transaction,  transaction_amount])
+def add_value(transaction_amount, last_transaction=[1]):
+    blockchain.append([last_transaction, transaction_amount])
     print(blockchain)
 
 
-add_value(5.4)
-add_value(0.1, get_last_blockchain_value())
-add_value(49.3, get_last_blockchain_value())
+def get_user_input():
+    return float( input("Your transaction amount please: "))
+
+tx_amount = get_user_input()
+add_value(tx_amount)
+tx_amount = get_user_input()
+add_value(tx_amount, get_last_blockchain_value())
+tx_amount = get_user_input()
+add_value(tx_amount, get_last_blockchain_value())
