9c4ba1f9eea85875e047415149807ff17ef7e47b
alexkummerer <alexander@thomas-kummerer.de>
2024-05-28 22:53:38 +0200

    verify blockchain

--- blockchain.py
+++ blockchain.py
@@ -19,7 +19,7 @@ def get_user_transaction():
     return float(input("Your transaction amount please: "))
 
 def get_user_choice():
-    user_input = int(input(' Your choice: '))
+    user_input = input(' Your choice: ')
     return user_input
 
 def print_block():
@@ -27,6 +27,14 @@ def print_block():
         print("Outputting Block")
         print(block)
     
+def verify_chain():
+    """Verifies the integrity of the blockchain."""
+    for index, block in enumerate(blockchain):
+        if index == 0:
+            continue
+        if block[0] != blockchain[index - 1]:
+            return False
+    return True
 
 def main():
     tx_amount = get_user_transaction()
@@ -35,15 +43,26 @@ def main():
         print("Please choose")
         print('1: Add a new transaction value')
         print("2: Output the blockchain blocks")
+        print("h: Manipulate the chain")
+        print("3: Quit!")
         user_choice = get_user_choice()
         print(user_choice)   
-        if user_choice == 1 :
+        if user_choice == '1' :
             tx_amount = get_user_transaction()
             add_transaction(tx_amount, get_last_block())
-        elif user_choice == 2:
+        elif user_choice == '2':
             print_block()
+        elif user_choice == 'h':
+            if len(blockchain) >= 1:
+                blockchain[0] = [2]
+        elif user_choice == 'q':
+            break
         else:
             print("Input was invalid, pls pick a value from the list!")
+        if not verify_chain():
+            print("Invalid blockchain")
+            break
+    print("Done")
 
 
 
